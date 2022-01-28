84775a95c63e8f1666eb5aeaf5422226ec14ee76
Joshue Jadie <joshue_jadie@dlsu.edu.ph>
2022-01-29 00:24:00 +0800

    Initial commit

---
+++ src/main/java/com/stadvdb/group22/mco2/MoviebaseApplication.java
@@ -0,0 +1,13 @@
+package com.stadvdb.group22.mco2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stadvdb/group22/mco2/Mco2TransactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stadvdb.group22.mco2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mco2TransactionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
